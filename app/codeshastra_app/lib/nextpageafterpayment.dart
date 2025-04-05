9f4a3ff52625cde880652642f7a5d40cf53c50c8
neelprajapati12 <neelkprajapati12@gmail.com>
2025-04-05 10:56:23 +0530

    initial commits of app

---
+++ app/codeshastra_app/lib/nextpageafterpayment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Nextpageafterpayment extends StatefulWidget {
+  const Nextpageafterpayment({super.key});
+
+  @override
+  State<Nextpageafterpayment> createState() => _NextpageafterpaymentState();
+}
+
+class _NextpageafterpaymentState extends State<Nextpageafterpayment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
